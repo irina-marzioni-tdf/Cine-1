8dbd60d8cda0069ba3a452add004bd2861890cef
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-05 19:51:48 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-function butacasRandom(butacas: boolean[], recorrido: number) {
+function butacasRandom(recorrido: number) {
+  let butacasAleatorias: boolean[] = new Array(recorrido);
   for (let indice = 0; indice < recorrido; indice++) {
-    butacas[indice] = Math.floor(Math.random() * 2);
+    let valorButaca: boolean;
+    let numeroAleatorio: number = Math.floor(Math.random() * 2);
+    if (numeroAleatorio === 1) {
+      valorButaca = true;
+    } else {
+      valorButaca = false;
+    }
+    butacasAleatorias[indice] = valorButaca;
   }
+  return butacasAleatorias;
 }
 
 function butacasDesocupadas(butacas: boolean[], recorrido: number) {
   let desocupadas: number = 0;
   for (let indice = 0; indice < recorrido; indice++) {
-    if (butacas[indice] === 0) {
+    if (butacas[indice] === false) {
       desocupadas++;
     }
   }
@@ -16,7 +25,7 @@ function butacasDesocupadas(butacas: boolean[], recorrido: number) {
 
 let numeroButacas: number = 150;
 let butacas: boolean[] = new Array(numeroButacas);
-butacasRandom(butacas, numeroButacas);
+butacas = butacasRandom(numeroButacas);
 console.log(
   "Las butacas desocupadas son: ",
   butacasDesocupadas(butacas, numeroButacas)
